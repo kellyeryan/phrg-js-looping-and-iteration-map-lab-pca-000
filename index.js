8fa801e41007d7bcde528e012de1c01d1bbeb55f
kellyeryan <kellyeryan@gmail.com>
2020-01-02 14:48:43 -0500

    Finish js looping and iteration map lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  const lowerCaseNames = drivers.map(function (name) {return name.toLowerCase(); });
+  return lowerCaseNames;
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(driver) {
+    const firstInName = driver.split(" ")[0];
+    const lastInName = driver.split(" ")[1];
+
+    return { firstName: firstInName, lastName: lastInName };
+  });
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`
+  });
+}
